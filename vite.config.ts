0363502c0d1b3a65a18064b0936553b29cb1c632
ObuchiYuki <yukibochi1@gmail.com>
2025-02-09 17:28:40 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "reactailwind"
+    : "./",
   plugins: [react()],
   resolve: {
     alias: {
